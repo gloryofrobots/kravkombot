102c5472fdb51bfd6ef5ec0831769679317f6fe7
gloryofrobots <gloryofrobots@gmail.com>
2019-06-02 20:37:49 +0300

    views

--- model.js
+++ model.js
@@ -2,6 +2,28 @@
 
 const { Pool } = require('pg');
 
+const GROUPS = [
+    {
+      id:1, name:"Крав мага с нуля",
+      price:1300,
+      equipment:["Перчатки для рукопашного боя", "Капа"],
+      address: `
+        г. Киев, ст. метро Университет, Вокзальная
+        ул. Жилянская, 75
+      `
+    },
+    {
+      id:2,
+      name:"Комбативз",
+      price:1300,
+      equipment:["Перчатки для рукопашного боя", "Капа", "Жесткие щитки на ноги"],
+      address: `
+        г. Киев, ст. метро Олимпийская, Институт Физкультуры
+        ул. Физкультуры, 1
+      `
+    }
+];
+
 const pool = new Pool({
   "connectionString": process.env.DATABASE_URL,
   "ssl": true
@@ -18,14 +40,9 @@ async function loadTest() {
     console.error(err);
     throw err;
   }
-
 }
-
 async function loadGroups() {
-  return [
-    {id:1, name:"Krav Maga", price:1300, equipment:["gloves", "mouth-piece"]},
-    {id:2, name:"Kombatives", price:1100, equipment:["gloves", "mouth-piece", "shin guards"]},
-  ];
+  return GROUPS;
 }
 
 async function loadGroup(id){
